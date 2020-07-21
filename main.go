d2849748dd9416db885388f929749309e4fabb86
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-07-21 19:02:10 +0700

    feat: Add IDE data scrapping feature

--- main.go
+++ main.go
@@ -1,82 +1,113 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-type informasiKuliah struct {
-	kode  string
-	nama  string
-	kelas string
+type MataKuliah struct {
+	Kode  string `json:"kode"`
+	Nama  string `json:"nama"`
+	Kelas string `json:"kelas"`
 }
 
-type ideData struct {
-	np         string
-	nama       string
-	mataKuliah []string
+type RiwayatTOEFL struct {
+	Listening int `json:"listening"`
+	Structure int `json:"structure"`
+	Reading   int `json:"reading"`
+	Total     int `json:"total"`
 }
 
-func scrapIde(id string) ideData {
-	c := colly.NewCollector(
-		colly.AllowedDomains(
-			"www.google.co.id",
-			"www.google.com",
-			"ide.unpar.ac.id",
-		),
-		colly.MaxDepth(1),
-	)
+type Mahasiswa struct {
+	Np           string         `json:"np"`
+	Foto         string         `json:"photo"`
+	Nama         string         `json:"nama"`
+	MataKuliah   []MataKuliah   `json:"mataKuliah"`
+	RiwayatTOEFL []RiwayatTOEFL `json:"riwayatTOEFL"`
+}
 
-	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
+func scrapIde(baseCollector colly.Collector, id string) Mahasiswa {
+	ideCollector := baseCollector.Clone()
 
-	// logging
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
+	ideCollector.AllowedDomains = []string{
+		"www.google.co.id",
+		"www.google.com",
+		"ide.unpar.ac.id",
+	}
 
-	c.OnError(func(r *colly.Response, err error) {
-		fmt.Println(err)
-	})
+	var result Mahasiswa
 
-	var result ideData
-
-	// visit IDE link
-	c.OnHTML("div.r", func(e *colly.HTMLElement) {
+	// visit IDE link from Google
+	ideCollector.OnHTML("div.r", func(e *colly.HTMLElement) {
 		link := e.ChildAttr("a", "href")
 
-		c.Visit(link)
+		ideCollector.Visit(link)
 	})
 
-	c.OnHTML(".page-header-headings", func(e *colly.HTMLElement) {
+	// Get mahasiswa picture
+	ideCollector.OnHTML(".page-header-image", func(e *colly.HTMLElement) {
+		picURL := e.ChildAttrs("a > img", "src")
+
+		result.Foto = picURL[0]
+	})
+
+	// Get mahasiswa np and name
+	ideCollector.OnHTML(".page-header-headings", func(e *colly.HTMLElement) {
 		identifier := e.ChildText("h1")
 
 		firstSpace := strings.Index(identifier, " ")
-		result.np = identifier[0:firstSpace]
-		result.nama = identifier[firstSpace+1:]
+		result.Np = identifier[0:firstSpace]
+		result.Nama = identifier[firstSpace+1:]
 	})
 
-	c.OnHTML(".node_category:nth-child(3) dd > ul > li", func(e *colly.HTMLElement) {
+	// Get current MK
+	ideCollector.OnHTML(".node_category:nth-child(3) dd > ul > li", func(e *colly.HTMLElement) {
 		text := e.ChildText("a")
+		textSplit := strings.Split(text, " ")
 
-		result.mataKuliah = append(result.mataKuliah, text)
+		mataKuliah := MataKuliah{
+			textSplit[0:1][0],
+			strings.Join(textSplit[1:len(textSplit)-1], " "),
+			textSplit[len(textSplit)-1:][0],
+		}
+
+		result.MataKuliah = append(result.MataKuliah, mataKuliah)
 	})
 
 	url := fmt.Sprintf("https://www.google.co.id/search?ie=UTF-8&q=%s", id)
 
-	c.Visit(url)
+	ideCollector.Visit(url)
 
 	return result
 }
 
 func main() {
-	ideData := scrapIde("2017730017")
+	// initalize base collector
+	baseCollector := colly.NewCollector(
+		colly.MaxDepth(1),
+	)
 
-	fmt.Println(ideData.nama)
-	fmt.Println(ideData.np)
+	// masquerade as a normal user
+	baseCollector.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
 
-	for _, text := range ideData.mataKuliah {
-		fmt.Println(text)
+	baseCollector.OnRequest(func(req *colly.Request) {
+		fmt.Println("Visiting: ", req.URL)
+	})
+
+	baseCollector.OnError(func(r *colly.Response, err error) {
+		fmt.Println(err)
+	})
+
+	ideData := scrapIde(*baseCollector, "2017730054")
+
+	jsonForm, err := json.Marshal(ideData)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	fmt.Println(string(jsonForm))
 }
