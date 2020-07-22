7cdadc8eff015ce1c9a78dc6bd16d17858cb970b
Cristopher Namchee <cristophernamchee12@gmail.com>
2020-07-22 10:12:36 +0700

    feat: Add TOEFL data scrapper

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gocolly/colly"
 )
@@ -15,30 +18,35 @@ type MataKuliah struct {
 }
 
 type RiwayatTOEFL struct {
-	Listening int `json:"listening"`
-	Structure int `json:"structure"`
-	Reading   int `json:"reading"`
-	Total     int `json:"total"`
+	Listening int       `json:"listening"`
+	Structure int       `json:"structure"`
+	Reading   int       `json:"reading"`
+	Total     int       `json:"total"`
+	Tanggal   time.Time `json:"tanggal"`
 }
 
 type Mahasiswa struct {
-	Np           string         `json:"np"`
+	Npm          string         `json:"npm"`
 	Foto         string         `json:"photo"`
 	Nama         string         `json:"nama"`
 	MataKuliah   []MataKuliah   `json:"mataKuliah"`
 	RiwayatTOEFL []RiwayatTOEFL `json:"riwayatTOEFL"`
 }
 
-func scrapIde(baseCollector colly.Collector, id string) Mahasiswa {
+func scrapIde(baseCollector colly.Collector, id string) (Mahasiswa, error) {
 	ideCollector := baseCollector.Clone()
 
 	ideCollector.AllowedDomains = []string{
 		"www.google.co.id",
-		"www.google.com",
 		"ide.unpar.ac.id",
 	}
 
+	var err error
 	var result Mahasiswa
+
+	ideCollector.OnError(func(_ *colly.Response, erro error) {
+		err = erro
+	})
 
 	// visit IDE link from Google
 	ideCollector.OnHTML("div.r", func(e *colly.HTMLElement) {
@@ -59,7 +67,7 @@ func scrapIde(baseCollector colly.Collector, id string) Mahasiswa {
 		identifier := e.ChildText("h1")
 
 		firstSpace := strings.Index(identifier, " ")
-		result.Np = identifier[0:firstSpace]
+		result.Npm = identifier[0:firstSpace]
 		result.Nama = identifier[firstSpace+1:]
 	})
 
@@ -81,7 +89,54 @@ func scrapIde(baseCollector colly.Collector, id string) Mahasiswa {
 
 	ideCollector.Visit(url)
 
-	return result
+	return result, err
+}
+
+func scrapTOEFL(baseCollector colly.Collector, id string) ([]RiwayatTOEFL, error) {
+
+	toeflCollector := baseCollector.Clone()
+
+	toeflCollector.AllowedDomains = []string{
+		"cdc.unpar.ac.id",
+	}
+
+	var err error
+	var result []RiwayatTOEFL
+
+	toeflCollector.OnHTML("table tbody", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(idx int, el *colly.HTMLElement) {
+			if idx > 0 {
+				listening, errL := strconv.Atoi(el.ChildText("td:nth-child(2)"))
+				structure, _ := strconv.Atoi(el.ChildText("td:nth-child(3)"))
+				reading, _ := strconv.Atoi(el.ChildText("td:nth-child(4)"))
+				total, _ := strconv.Atoi(el.ChildText("td:nth-child(5)"))
+				date, _ := time.Parse("2 January 2006", el.ChildText("td:nth-child(6)"))
+
+				if errL == nil {
+					result = append(
+						result,
+						RiwayatTOEFL{
+							listening,
+							structure,
+							reading,
+							total,
+							date,
+						},
+					)
+				}
+			}
+		})
+	})
+
+	err = toeflCollector.Post(
+		"http://cdc.unpar.ac.id/old/cek-nilai-toefl/",
+		map[string]string{
+			"npm": id,
+			"act": "Cek Nilai",
+		},
+	)
+
+	return result, err
 }
 
 func main() {
@@ -93,15 +148,18 @@ func main() {
 	// masquerade as a normal user
 	baseCollector.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36"
 
-	baseCollector.OnRequest(func(req *colly.Request) {
-		fmt.Println("Visiting: ", req.URL)
-	})
+	ideData, err := scrapIde(*baseCollector, "2016730011")
 
-	baseCollector.OnError(func(r *colly.Response, err error) {
+	if err == nil && len(ideData.Npm) > 0 {
+		toeflData, erro := scrapTOEFL(*baseCollector, "2016730011")
+
+		if erro == nil {
+			ideData.RiwayatTOEFL = toeflData
+		}
+	} else {
 		fmt.Println(err)
-	})
-
-	ideData := scrapIde(*baseCollector, "2017730054")
+		os.Exit(1)
+	}
 
 	jsonForm, err := json.Marshal(ideData)
 	if err != nil {
